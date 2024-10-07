0168ff0ff078217af673318bf01962fb69613540
alpha <alpha5611331@gmail.com>
2024-10-07 02:53:43 -0400

    index by year

--- indexer.py
+++ indexer.py
@@ -1,82 +1,135 @@
-import time
 import json
+import sys
+import time
+from datetime import datetime, timedelta
 from pathlib import Path
 
 from loguru import logger
 from playwright.sync_api import sync_playwright
 
 CUR_PATH = Path(__file__).parent
-OUTPUT_PATH = CUR_PATH / "index"
-OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
+INDEX_DIR_PATH = CUR_PATH / "index"
+INDEX_DIR_PATH.mkdir(parents=True, exist_ok=True)
 
 HOME_LINK = "https://mauwi.wycokck.org/CitizenAccess/Cap/CapHome.aspx?module=Building&TabName=Building&TabList=Home%7C0%7CBuilding%7C1%7CEnforce%7C2%7CLicenses%7C3%7CPlanning%7C4%7CCurrentTabIndex%7C1"
 
 
-def work():
+def work(year: int):
     with sync_playwright() as pw_ctx_man:
         browser = pw_ctx_man.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
 
-        page.goto(HOME_LINK, wait_until="networkidle")
+        # Start date
+        start_date = datetime(year=year, month=1, day=1)
 
-        # fill query field
-        page.eval_on_selector(
-            "input#ctl00_PlaceHolderMain_generalSearchForm_txtGSStartDate",
-            'el => el.value = "01/01/2020"',
-        )
-        # click search button
-        page.query_selector("a#ctl00_PlaceHolderMain_btnNewSearch").click()
+        # End date (current date)
+        end_date = datetime.now()
 
-        page_number = 0
-        while True:
+        # Generate and print each date from start to end
+        current_date = start_date
+        while current_date <= end_date:
             try:
-                logger.info(f"page_number: {page_number}")
-
-                # fetch table rows
-                page.wait_for_selector('td[style="width:110px;"]')
-                time.sleep(0.5)
-                elems = page.query_selector_all('td[style="width:110px;"]')
-                if len(elems) == 0:
+                if current_date.year > year:
                     break
 
-                # iterate rows
-                for elem in elems:
-                    permit_number = elem.inner_text().strip()
-                    elem.wait_for_selector("a", state="attached")
-                    link = elem.query_selector("a").get_attribute("href")
+                date_str = current_date.strftime("%m/%d/%Y")
+                logger.info(f"date: {date_str}")
 
-                    # save `permit_number` and `link``
-                    output_file_path = OUTPUT_PATH / f"{permit_number}.json"
-                    with output_file_path.open("w") as file:
-                        json.dump(
-                            {
-                                "permit_number": permit_number,
-                                "link": link,
-                            },
-                            file,
+                output_fname = date_str.replace("/", "-") + ".jsonl"
+                output_fpath = INDEX_DIR_PATH / output_fname
+
+                already_done = False
+                if output_fpath.is_file():
+                    with output_fpath.open("r") as file:
+                        try:
+                            damaged = False
+                            for line in file:
+                                info = json.loads(line)
+                                if "permit_number" not in info:
+                                    damaged = True
+                                    break
+                                if "link" not in info:
+                                    damaged = True
+                                    break
+                            if not damaged:
+                                already_done = True
+                                logger.info("already done")
+                        except:  # noqa: E722
+                            pass
+
+                if not already_done:
+                    page.goto(HOME_LINK, wait_until="networkidle")
+
+                    # fill query field
+                    page.eval_on_selector(
+                        "input#ctl00_PlaceHolderMain_generalSearchForm_txtGSStartDate",
+                        f'el => el.value = "{date_str}"',
+                    )
+                    page.eval_on_selector(
+                        "input#ctl00_PlaceHolderMain_generalSearchForm_txtGSEndDate",
+                        f'el => el.value = "{date_str}"',
+                    )
+                    # click search button
+                    page.query_selector("a#ctl00_PlaceHolderMain_btnNewSearch").click()
+
+                    index_list = []
+                    page_number = 0
+                    while True:
+                        logger.info(f"{date_str}, page_number: {page_number}")
+
+                        # wait for page loading
+                        page.wait_for_load_state(state="networkidle")
+                        display_style = ""
+                        while display_style != "none":
+                            display_style = page.evaluate("""() => {
+                                const element = document.querySelector('#divGlobalLoading');
+                                return window.getComputedStyle(element).display;
+                            }""")
+                            time.sleep(0.1)
+                        time.sleep(0.5)
+
+                        # fetch table rows
+                        elems = page.query_selector_all('td[style="width:110px;"]')
+                        if len(elems) == 0:
+                            break
+
+                        # iterate rows
+                        for elem in elems:
+                            permit_number = elem.inner_text().strip()
+                            elem.wait_for_selector("a", state="attached")
+                            link = elem.query_selector("a").get_attribute("href")
+
+                            index_list.append(
+                                {
+                                    "permit_number": permit_number,
+                                    "link": link,
+                                }
+                            )
+
+                        # click next button
+                        page_btns = page.query_selector_all(
+                            "a.aca_simple_text.font11px"
                         )
+                        if len(page_btns) >= 1:
+                            next_btn = page_btns[-1]
+                            if "Next" in next_btn.inner_text():
+                                next_btn.click()
+                            else:
+                                break
+                        else:
+                            break
+
+                        page_number += 1
+
+                    with output_fpath.open("w") as file:
+                        for index in index_list:
+                            file.write(json.dumps(index) + "\n")
 
             except Exception as ex:
                 logger.exception(ex)
 
-            # click next button
-            page.wait_for_selector("a.aca_simple_text.font11px", state="attached")
-            page_btns = page.query_selector_all("a.aca_simple_text.font11px")
-            next_btn = page_btns[-1]
-            next_btn.click()
-
-            # wait for page loading
-            page.wait_for_load_state(state="networkidle")
-            display_style = ""
-            while display_style != "none":
-                display_style = page.evaluate("""() => {
-                    const element = document.querySelector('#divGlobalLoading');
-                    return window.getComputedStyle(element).display;
-                }""")
-                time.sleep(0.1)
-
-            page_number += 1
+            current_date += timedelta(days=1)
 
         context.close()
         browser.close()
@@ -84,7 +137,10 @@ def work():
 
 def main():
     try:
-        work()
+        if len(sys.argv) > 1:
+            for arg in sys.argv[1:]:
+                year = int(arg)
+                work(year=year)
     except Exception as ex:
         logger.exception(ex)
 
