e1ea481c9fa19b7904e32d414dc330661b507bc1
alpha <alpha5611331@gmail.com>
2024-10-07 10:48:56 -0400

    changed output table columns

--- merge_result.py
+++ merge_result.py
@@ -27,7 +27,8 @@ def regulate_str(input_str: str) -> str:
             regulated_lines.append(line)
             previous_line_empty = False
 
-    return "\n".join(regulated_lines)
+    ret = "\n".join(regulated_lines)
+    return ret.strip()
 
 
 def merge():
@@ -44,8 +45,16 @@ def merge():
                 "short_notes": [],
                 "link": [],
                 "work_location": [],
-                "applicant": [],
-                "licensed_professional": [],
+                "applicant_email": [],
+                "applicant_home_phone": [],
+                "applicant_mobile_phone": [],
+                "applicant_fax": [],
+                "applicant_others": [],
+                "licensed_professional_email": [],
+                "licensed_professional_home_phone": [],
+                "licensed_professional_mobile_phone": [],
+                "licensed_professional_fax": [],
+                "licensed_professional_others": [],
                 "project_description": [],
             }
         )
@@ -86,23 +95,91 @@ def merge():
                                     "",
                                 )
                             )
-                            store_info["applicant"] = regulate_str(
-                                info.get(
-                                    "applicant",
-                                    "",
-                                )
+
+                            # parse applicant information
+                            applicant = regulate_str(info.get("applicant", ""))
+                            if applicant != "":
+                                lines = applicant.split("\n")
+                                for i, line in enumerate(lines):
+                                    if "@" in line.strip():
+                                        store_info["applicant_email"] = (
+                                            line.strip().strip(".")
+                                        )
+                                        applicant = applicant.replace(line, "")
+                                    if line.strip() == "Home Phone:":
+                                        if "@" not in lines[i + 1]:
+                                            store_info["applicant_home_phone"] = lines[
+                                                i + 1
+                                            ].strip()
+                                            applicant = applicant.replace(
+                                                line, ""
+                                            ).replace(lines[i + 1], "")
+                                    if line.strip() == "Mobile Phone:":
+                                        if "@" not in lines[i + 1]:
+                                            store_info["applicant_mobile_phone"] = (
+                                                lines[i + 1].strip()
+                                            )
+                                            applicant = applicant.replace(
+                                                line, ""
+                                            ).replace(lines[i + 1], "")
+                                    if line.strip() == "Fax:":
+                                        if "@" not in lines[i + 1]:
+                                            store_info["applicant_fax"] = lines[
+                                                i + 1
+                                            ].strip()
+                                            applicant = applicant.replace(
+                                                line, ""
+                                            ).replace(lines[i + 1], "")
+
+                            store_info["applicant_others"] = regulate_str(applicant)
+
+                            # parse licensed_professional information
+                            licensed_professional = regulate_str(
+                                info.get("licensed_professional", "")
                             )
-                            store_info["licensed_professional"] = regulate_str(
-                                info.get(
-                                    "licensed_professional",
-                                    "",
-                                )
-                            )
-                            store_info["project_description"] = regulate_str(
-                                info.get(
-                                    "project_description",
-                                    "",
-                                )
+                            if licensed_professional != "":
+                                lines = licensed_professional.split("\n")
+                                for i, line in enumerate(lines):
+                                    if "@" in line.strip():
+                                        store_info["licensed_professional_email"] = (
+                                            line.strip().strip(".")
+                                        )
+                                        licensed_professional = (
+                                            licensed_professional.replace(line, "")
+                                        )
+                                    if line.strip() == "Home Phone:":
+                                        if "@" not in lines[i + 1]:
+                                            store_info[
+                                                "licensed_professional_home_phone"
+                                            ] = lines[i + 1].strip()
+                                            licensed_professional = (
+                                                licensed_professional.replace(
+                                                    line, ""
+                                                ).replace(lines[i + 1], "")
+                                            )
+                                    if line.strip() == "Mobile Phone:":
+                                        if "@" not in lines[i + 1]:
+                                            store_info[
+                                                "licensed_professional_mobile_phone"
+                                            ] = lines[i + 1].strip()
+                                            licensed_professional = (
+                                                licensed_professional.replace(
+                                                    line, ""
+                                                ).replace(lines[i + 1], "")
+                                            )
+                                    if line.strip() == "Fax:":
+                                        if "@" not in lines[i + 1]:
+                                            store_info["licensed_professional_fax"] = (
+                                                lines[i + 1].strip()
+                                            )
+                                            licensed_professional = (
+                                                licensed_professional.replace(
+                                                    line, ""
+                                                ).replace(lines[i + 1], "")
+                                            )
+
+                            store_info["licensed_professional_others"] = regulate_str(
+                                licensed_professional
                             )
 
                     res_df = pd.concat(
